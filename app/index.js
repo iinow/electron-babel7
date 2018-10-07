17d937b71ff69959ec427dc1b77b03078ca622c1
iinow <iinow@naver.com>
2018-10-07 09:26:42 +0900

    init commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/renderer/App'
+import Login from './src/renderer/Login'
+// const app = () => {
+//     return (
+//         <div>
+//             <button>??</button>
+//         </div>
+//     )
+// }
+
+ReactDOM.render(<Login/>, document.getElementById('root'))
